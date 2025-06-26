cdbdee584f75a6c5d12aa279509b0e8ad3134582
FranzinLuca <lucafranzin00@gmail.com>
2025-06-26 11:39:50 +0200

    cleaned code + BTAD dataloader

--- source/dataLoader.py
+++ source/dataLoader.py
@@ -1,17 +1,14 @@
 import os
-import pandas as pd
-import matplotlib.pyplot as plt
-import kagglehub
 import glob
-import numpy as np
 from tqdm.notebook import tqdm
-from collections import Counter
-from torchvision.datasets import ImageFolder
-from torchvision import transforms
+import torch
 from torch.utils.data import Dataset, DataLoader
-import random
 from PIL import Image
-from torch.utils.data import random_split
+from torchvision import transforms
+
+# ------------------------------------------------ #
+# MVTEC_AD #
+# ------------------------------------------------ #
 
 class Dataset_MVTEC_AD(Dataset):
     def __init__(self,root_dir,mode='train',transform=None,augment=False,aug_prob=0.5):
@@ -91,25 +88,89 @@ class Dataset_MVTEC_AD(Dataset):
          #   img=apply_random_augmentation(img,self.aug_prob)
         return img,label
 
-transform = transforms.Compose([
-    transforms.Resize((512,512)), # Resize images to 512x512
-    transforms.ToTensor() # Convert images to PyTorch tensors
-])
-# Download the MVTec AD dataset from Kaggle
-path = kagglehub.dataset_download("ipythonx/mvtec-ad")
-print("Path to dataset files:", path)
+def load_MVTEC_AD(main_path, transform, batch_size=32):
+    # Download the MVTec AD dataset from Kaggle
+    #path = kagglehub.dataset_download("ipythonx/mvtec-ad")
+    #print("Path to dataset files:", path)
 
-# train and test dataset paths
-train_dataset=Dataset_MVTEC_AD(path,mode="train",transform=transform,augment=False)
-test_dataset=Dataset_MVTEC_AD(path,mode="test",transform=transform,augment=False)
+    if transform is None:
+        transform = transforms.Compose([
+            transforms.Resize((512, 512)),
+            transforms.ToTensor(),
+        ])
 
-train_len = int(0.8 * len(train_dataset))
-val_len = len(train_dataset) - train_len
-# Split the training dataset into training and validation subsets
-train_subset, val_subset = random_split(train_dataset, [train_len, val_len])
+    # train and test dataset paths
+    train_dataset=Dataset_MVTEC_AD(main_path,mode="train",transform=transform)
+    test_dataset=Dataset_MVTEC_AD(main_path,mode="test",transform=transform)
+
+    # Create DataLoader for training,validation and test datasets
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset,batch_size=batch_size,shuffle=False)
+    return train_loader, test_loader
 
 
-# Create DataLoader for training,validation and test datasets
-train_loader = DataLoader(train_subset, batch_size=64, shuffle=True)
-val_loader = DataLoader(val_subset, batch_size=1, shuffle=False)
-test_loader=DataLoader(test_dataset,batch_size=64,shuffle=False)
+
+# ------------------------------------------------ #
+# BTAD #
+# ------------------------------------------------ #
+class load_dataset_BTAD(Dataset):
+    def __init__(self, image_paths, transform = None, ground_truth_paths=[]):
+        self.paths = image_paths
+        self.ground_truth_paths = ground_truth_paths
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, i):
+        path_string = self.paths[i]
+        img = Image.open(path_string).convert('RGB')
+        if self.transform:
+            img=self.transform(img)
+        
+        if "ko" in path_string:
+            label = 0
+        elif "ok" in path_string:
+            label = 1
+
+        if len(self.ground_truth_paths)>0:
+            img_gt = Image.open(self.ground_truth_paths[i]).convert('RGB')
+            if self.transform:
+                img_gt=self.transform(img_gt)
+        else:
+            img_gt = torch.zeros((1, 512, 512))
+        
+        return img, label, img_gt
+    
+def load_train_paths_BTAD(main_folder):
+    subfolders = os.listdir(main_folder)
+    train_folder_paths = [f"{main_folder}/{base_path}/train/ok" for base_path in subfolders]
+    img_train_paths = [f"{path}/{img}" for path in train_folder_paths for img in os.listdir(path)]
+    return img_train_paths, _
+
+def load_test_paths_BTAD(main_folder):
+    subfolders = os.listdir(main_folder)
+    test_folder_paths_ko = [f"{main_folder}/{base_path}/test/ko" for base_path in subfolders]
+    test_folder_paths_ok = [f"{main_folder}/{base_path}/test/ok" for base_path in subfolders]
+    gt_folder_paths_ko = [f"{main_folder}/{base_path}/ground_truth/ko" for base_path in subfolders]
+    test_folder_paths = test_folder_paths_ok + test_folder_paths_ko
+    img_test_paths = [f"{path}/{img}" for path in test_folder_paths for img in os.listdir(path)]
+    img_gt_paths = [f"{path}/{img}" for path in gt_folder_paths_ko for img in os.listdir(path)]
+    return img_test_paths, img_gt_paths
+
+def load_BTAD(main_path, transform=None, batch_size=32):
+    train_paths, _ = load_train_paths_BTAD(main_path)
+    test_paths, gt_paths = load_test_paths_BTAD(main_path)
+    
+    if transform is None:
+        transform = transforms.Compose([
+            transforms.Resize((512, 512)),
+            transforms.ToTensor(),
+        ])
+    
+    dataset_train = load_dataset_BTAD(train_paths, transform=transform)
+    dataset_test = load_dataset_BTAD(test_paths, transform=transform, ground_truth_paths=gt_paths)
+    
+    train_dataloader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(dataset_test, batch_size=batch_size, shuffle=False)
+    return train_dataloader, test_dataloader
